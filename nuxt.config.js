2b3e0d4518342a42ae11d228cc49d255702ecc12
Piotr Perzyna <piotr.perzyna@gmail.com>
2020-12-23 09:41:19 +0100

    add icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,7 @@ export default {
   head: {
     titleTemplate: `%s - Kej's recipes`,
     meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    link: [{ rel: 'stylesheet', href: 'https://cdn.materialdesignicons.com/5.3.45/css/materialdesignicons.min.css' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
